a5b67702158d8a9f20e0b68f5f6dfc134cf79b36
corikachu <konomama0505@gmail.com>
2015-04-10 21:15:45 +0900

    create base application

---
+++ app/src/main/java/com/yebimom/android/yebimom/MainApp.java
@@ -0,0 +1,15 @@
+package com.yebimom.android.yebimom;
+
+import android.app.Application;
+
+/**
+ * com.yebimom.android.yebimom Need Comment!
+ */
+public class MainApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
