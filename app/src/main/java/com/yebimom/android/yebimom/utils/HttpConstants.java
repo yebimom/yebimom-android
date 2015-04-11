256bbdef3f78a7207be6900015019e17853d1a6e
corikachu <konomama0505@gmail.com>
2015-04-11 16:39:28 +0900

    create Http Constants

--- app/src/main/java/com/yebimom/android/yebimom/utils/HttpConstants.java
+++ app/src/main/java/com/yebimom/android/yebimom/utils/HttpConstants.java
@@ -3,6 +3,8 @@ package com.yebimom.android.yebimom.utils;
 /**
  * com.yebimom.android.yebimom.utils Need Comment!
  */
-public class HttpConstants {
-    
+public interface HttpConstants {
+
+    String LOGIN_URL = "http://192.168.52.101:8000/api/login/";
+
 }
