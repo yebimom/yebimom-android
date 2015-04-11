ea2c9932a6c961eb442c600fbdd1ffebc1fa34cc
corikachu <konomama0505@gmail.com>
2015-04-11 16:40:34 +0900

    create JwtAuthLoginToken class

--- app/src/main/java/com/yebimom/android/yebimom/authentication/JwtAuthLoginToken.java
+++ app/src/main/java/com/yebimom/android/yebimom/authentication/JwtAuthLoginToken.java
@@ -1,8 +1,68 @@
 package com.yebimom.android.yebimom.authentication;
 
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.yebimom.android.yebimom.utils.HttpLogin;
+
 /**
- * com.yebimom.android.yebimom.authentication Need Comment!
+ * JwtAuthLoginToken class used to singleton pattern
  */
 public class JwtAuthLoginToken {
-    
+
+    private volatile static JwtAuthLoginToken instance;
+
+    private final static String TAG = JwtAuthLoginToken.class.getSimpleName();
+
+    private String loginJson = null;
+    private String loginToken = null;
+
+    private JwtAuthLoginToken() {
+
+    }
+
+    @Nullable
+    public String createLoginToken(String id, String password){
+
+        HttpLogin httpLogin = HttpLogin.create()
+                                      .setUsername(id)
+                                      .setPassword(password)
+                                      .addQueue();
+
+        if(httpLogin.isLogin()){
+            loginToken = httpLogin.getToken();
+            Log.d(TAG, loginToken);
+        }else{
+            Log.d(TAG, "Login Error");
+        }
+
+        return loginToken;
+    }
+
+    public static JwtAuthLoginToken getInstance() {
+        if (instance == null){
+            synchronized(JwtAuthLoginToken.class) {
+                if (instance == null) {
+                    instance = new JwtAuthLoginToken();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public String getLoginJson() {
+        return loginJson;
+    }
+
+    public void setLoginJson(String loginJson) {
+        this.loginJson = loginJson;
+    }
+
+    public String getLoginToken() {
+        return loginToken;
+    }
+
+    public void setLoginToken(String loginToken) {
+        this.loginToken = loginToken;
+    }
 }
