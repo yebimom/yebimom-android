e7d7dff6e8852c15ce7b296d8ff5254980d7aed0
corikachu <konomama0505@gmail.com>
2015-04-11 16:41:34 +0900

    add feature get login token

--- app/src/main/java/com/yebimom/android/yebimom/ui/Login/LoginFragment.java
+++ app/src/main/java/com/yebimom/android/yebimom/ui/Login/LoginFragment.java
@@ -1,4 +1,4 @@
-package com.yebimom.android.yebimom.ui.Login;
+package com.yebimom.android.yebimom.ui.login;
 
 
 import android.app.Fragment;
@@ -10,27 +10,39 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.yebimom.android.yebimom.R;
+import com.yebimom.android.yebimom.authentication.JwtAuthLoginToken;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class LoginFragment extends Fragment{
 
+    private final static String TAG = LoginFragment.class.getSimpleName();
+
     @InjectView(R.id.loginId) EditText mLoginId;
     @InjectView(R.id.loginPassword) EditText mPassword;
-    @InjectView(R.id.loginButton) Button mLoginButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Create Fragment View with fragment_login.xml
+        // Create fragment view with fragment_login.xml
         View rootView = inflater.inflate(R.layout.fragment_login, container, false);
 
         // ButterKnife init
         ButterKnife.inject(this, rootView);
 
-
         return rootView;
+    }
+
+    @OnClick(R.id.loginButton)
+    public void loginButton(Button button){
+        String loginId = mLoginId.getText().toString();
+        String loginPassword = mPassword.getText().toString();
+
+        JwtAuthLoginToken jwtAuthLoginToken = JwtAuthLoginToken.getInstance();
+        jwtAuthLoginToken.createLoginToken(loginId, loginPassword);
+
     }
 
     @Override
