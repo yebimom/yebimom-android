ed5d7dd2446be8e482d434c24948e2588a4b30a6
corikachu <konomama0505@gmail.com>
2015-04-19 23:08:36 +0900

    update login actionbar

--- app/src/main/java/com/yebimom/android/yebimom/ui/Login/LoginFragment.java
+++ app/src/main/java/com/yebimom/android/yebimom/ui/Login/LoginFragment.java
@@ -1,13 +1,22 @@
 package com.yebimom.android.yebimom.ui.login;
 
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toolbar;
 
 import com.yebimom.android.yebimom.R;
 import com.yebimom.android.yebimom.authentication.JwtAuthLoginToken;
@@ -16,7 +25,7 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
-public class LoginFragment extends Fragment{
+public class LoginFragment extends Fragment {
 
     private final static String TAG = LoginFragment.class.getSimpleName();
 
@@ -31,6 +40,12 @@ public class LoginFragment extends Fragment{
 
         // ButterKnife init
         ButterKnife.inject(this, rootView);
+
+        ActionBar actionbar = ((ActionBarActivity)getActivity()).getSupportActionBar();
+
+        if(actionbar != null){
+            actionbar.show();
+        }
 
         return rootView;
     }
