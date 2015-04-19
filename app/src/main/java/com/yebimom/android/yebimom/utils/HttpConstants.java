02d75be47b9443e015b52d480333201f6b3fc2a7
corikachu <konomama0505@gmail.com>
2015-04-19 23:10:16 +0900

    add HttpConstants CenterAPIURL

--- app/src/main/java/com/yebimom/android/yebimom/utils/HttpConstants.java
+++ app/src/main/java/com/yebimom/android/yebimom/utils/HttpConstants.java
@@ -6,5 +6,6 @@ package com.yebimom.android.yebimom.utils;
 public interface HttpConstants {
 
     String LOGIN_URL = "http://192.168.52.101:8000/api/login/";
+    String CENTER_API_URL = "http://api.androidhive.info/json/movies.json";
 
 }
