94e1c7ae66d2dbc6dcbb8110fab792d13e3dd4f9
corikachu <konomama0505@gmail.com>
2015-04-20 16:50:12 +0900

    delete unused import

--- app/src/main/java/com/yebimom/android/yebimom/ui/Login/LoginFragment.java
+++ app/src/main/java/com/yebimom/android/yebimom/ui/Login/LoginFragment.java
@@ -3,20 +3,13 @@ package com.yebimom.android.yebimom.ui.login;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toolbar;
 
 import com.yebimom.android.yebimom.R;
 import com.yebimom.android.yebimom.authentication.JwtAuthLoginToken;
@@ -29,8 +22,10 @@ public class LoginFragment extends Fragment {
 
     private final static String TAG = LoginFragment.class.getSimpleName();
 
-    @InjectView(R.id.loginId) EditText mLoginId;
-    @InjectView(R.id.loginPassword) EditText mPassword;
+    @InjectView(R.id.loginId)
+    EditText mLoginId;
+    @InjectView(R.id.loginPassword)
+    EditText mPassword;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,9 +36,9 @@ public class LoginFragment extends Fragment {
         // ButterKnife init
         ButterKnife.inject(this, rootView);
 
-        ActionBar actionbar = ((ActionBarActivity)getActivity()).getSupportActionBar();
+        ActionBar actionbar = ((ActionBarActivity) getActivity()).getSupportActionBar();
 
-        if(actionbar != null){
+        if (actionbar != null) {
             actionbar.show();
         }
 
@@ -51,7 +46,7 @@ public class LoginFragment extends Fragment {
     }
 
     @OnClick(R.id.loginButton)
-    public void loginButton(Button button){
+    public void loginButton(Button button) {
         String loginId = mLoginId.getText().toString();
         String loginPassword = mPassword.getText().toString();
 
